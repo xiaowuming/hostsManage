76f6f235e29c4537126b9202a4842dec067abf03
肖武明 <xiaowuming@gmail.com>
2016-05-28 21:40:21 +0800

    添加NPM命令管理

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -11,9 +10,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
--- cmd.js
+++ cmd.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+
+var program = require('commander'),
+    fs = require('fs'),
+    open = require('open');
 
 program
     .version(require("./package.json").version)
@@ -11,4 +14,38 @@ var commander = process.argv;
 var appoint = process.argv[2];
 if (commander.length < 3) {
     appoint = "start";
+}
+
+// 检测sudo权限
+fs.writeFile('/etc/.hostsManage', 'TEST', function (err, date) {
+    if (err) {
+        console.log('你不是使用[sudo]权限启动HostsManage,将提供只读服务.');
+        setTimeout(function () {
+            console.log('使用"sudo hosts-manage start"可以完整的体验本服务.');
+        }, 1000);
+        setTimeout(function () {
+            console.log('正在尝试启动只读服务...');
+        }, 2000);
+        setTimeout(function () {
+            startServer();
+            stopTips();
+        }, 3000);
+    } else {
+        fs.unlink('/etc/.hostsManage');
+        startServer();
+        stopTips();
+    }
+
+});
+
+
+function startServer() {
+    require('./bin/start')(function () {
+        console.log('自动打开管理页面,如果打开失败,请手动访问:http://127.0.0.1:9123');
+        open('http://127.0.0.1:9123');
+    })
+}
+
+function stopTips() {
+    console.log('服务已经启动,要停止服务请按"Command + C"');
 }
